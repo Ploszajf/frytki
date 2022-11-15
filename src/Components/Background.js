0d46c8fdf15778eec0835e39f259a096f0e0d402
Ploszajf <72620915+Ploszajf@users.noreply.github.com>
2022-11-15 15:20:09 +0100

    Update Background.js

--- src/Components/Background.js
+++ src/Components/Background.js
@@ -8,7 +8,9 @@ export class Background extends Component {
             justifyContent: 'center',
             widty: 100 + '%',
             height: 100 + 'vh',
-            backgroundImage: 'url("https://bi.im-g.pl/im/31/79/14/z21470513Q,Mcdonald-s-przy-ul---Wojska-Polskiego-w-Zielonej-G.jpg")'
+            backgroundImage: 'url("https://bi.im-g.pl/im/31/79/14/z21470513Q,Mcdonald-s-przy-ul---Wojska-Polskiego-w-Zielonej-G.jpg")',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center'
 
         }
 
