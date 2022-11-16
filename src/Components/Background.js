15a647f742c7aa3f6a245fd39b23f3b30689aa74
Ploszajf <72620915+Ploszajf@users.noreply.github.com>
2022-11-16 13:34:41 +0100

    background

--- src/Components/Background.js
+++ src/Components/Background.js
@@ -8,7 +8,7 @@ export class Background extends Component {
             justifyContent: 'center',
             widty: 100 + '%',
             height: 100 + 'vh',
-            backgroundImage: 'url("https://scontent-waw1-1.xx.fbcdn.net/v/t39.30808-6/239561328_4913147785367213_7461726983045316360_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=e3f864&_nc_ohc=xad5O0APcZ0AX_iLpGE&_nc_ht=scontent-waw1-1.xx&oh=00_AfBjxyrkHwoaaSrTjOWNFdZjEwyCq_PzFy8m4rVkXeSA-Q&oe=63782C56")',
+            backgroundImage: 'url("../images/background.jpg")',
             backgroundSize: 'cover',
             backgroundPosition: 'center'
 
